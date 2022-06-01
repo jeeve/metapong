4a44d87413136b0b7aa31a8fd074dbaf608c5d98
jeeve <jyvaisj@gmail.com>
2022-06-01 22:46:04 +0200

    bodyparser

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
 app.get('/', function(request, response) {
    response.sendFile('./index.html');
