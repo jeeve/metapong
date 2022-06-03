cb563d04ae36f07f2b112e032a7de73653fb2329
jeeve <jyvaisj@gmail.com>
2022-06-03 23:19:01 +0200

    simplifie

--- public/js/api.js
+++ public/js/api.js
@@ -1,33 +1,3 @@
-function notifieServeur(message) {
-  const balle = document.querySelector("circle");
-  const vx = parseFloat(balle.getAttribute("vx"));
-  const vy = parseFloat(balle.getAttribute("vy"));
-
-  const cx = parseFloat(balle.getAttribute("cx"));
-  const cy = parseFloat(balle.getAttribute("cy"));
-
-  const init = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      id: ID,
-      message: message,
-      cx: cx,
-      cy: cy,
-      vx: vx,
-      vy: vy,
-    }),
-    mode: "cors",
-    credentials: "same-origin",
-  };
-
-  fetch("http://localhost:5500/clients/", init).then(() => {
-    console.log("data envoyée");
-  });
-}
-
 async function register() {
 
   let id = 0;
@@ -100,28 +70,5 @@ async function getBalle(id) {
 
   return balle;
 }
-/*
-function register(ID) {
-
-  const init = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      id: ID,
-    }),
-    mode: "cors",
-    credentials: "same-origin",
-  };
-
-  fetch("http://localhost:5500/register/", init).then(response => response.json())
-   .then(response => document.querySelector("#ecran").innerHTML = JSON.stringify(response));
-
-}
-*/
-
-//  fetch("http://localhost:5500/quisuisje/", init).then(response => response.json())
-//  .then(response => document.querySelector("#ecran").innerHTML = JSON.stringify(response));
 
 
